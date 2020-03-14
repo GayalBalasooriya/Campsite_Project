33adc5a73e58a4572ff510e2aa819dc57b5b554a
Gayal Balasooriya <baigayal01@gmail.com>
2020-03-14 19:09:38 +0530

    second commit

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTY0W4OItYAO9HerX-Dtxtg0SlHq8kyqtWnbBdseRIeWM9kJSgf"},
+    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSi0nefdUYfOEp7WxiyghRkL1PxA2JobjAdyYHi8A4bDU9zWQpN"},
+    {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTPobV4NI_C_OVJrP97BjBY8Vz2oFDxcCUNcY5hvjnygLLUskqb"}
+]
 
 app.get("/", function(req, res) {
     res.render("landing")
 })
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
-        {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT574j7lIqe5VIbPdF8RvAcBitKcYF0HFeqUa2mspav9oQ4aCCG"},
-        {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTPobV4NI_C_OVJrP97BjBY8Vz2oFDxcCUNcY5hvjnygLLUskqb"}
-    ]
     res.render("campgrounds", {campgrounds: campgrounds})
 })
+
+app.post("/campgrounds", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds")
+
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
+});
 
 app.listen(3000, function() {
     console.log("The Yelp Camp has started")
