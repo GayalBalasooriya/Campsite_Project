9b991270171cd86a5d67e6d4d0711c99d92b2333
Gayal Balasooriya <baigayal01@gmail.com>
2020-03-23 22:55:19 +0530

    sixth commit

--- v6/app.js
+++ v6/app.js
@@ -28,6 +28,11 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 // Campground.create(
 //     {
 //         name: "Granite Hill", 
@@ -59,12 +64,13 @@ app.get("/", function(req, res) {
 //INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
     //res.render("campgrounds", {campgrounds: campgrounds})
+    
     Campground.find({}, function(err, allCampgrounds) {
         if(err) {
             console.log(err);
         }
         else {
-            res.render("campgrounds/index", {campgrounds: allCampgrounds})
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
         }
     });
 });
@@ -109,7 +115,7 @@ app.get("/campgrounds/:id", function(req, res) {
 });
 
 // COMMENTS ROUTE
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
     // Find campground by id
     Campground.findById(req.params.id, function(err, campground) {
         if(err) {
@@ -121,7 +127,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
     });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
     // Lookup campground using ID
     // Create new comment
     // Connect new comment to campground
@@ -168,6 +174,31 @@ app.post("/register", function(req, res) {
     })
 });
 
+// show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+// app.post("/login", middleware, callback)
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "campgrounds",
+    failureRedirect: "/login"
+}), function(req, res) {
+    
+});
+
+// logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function() {
     console.log("The Yelp Camp has started")
